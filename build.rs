fa6de1844c601c33b167a310654e88c9a3be57a9
Peter Mezei <mezeipetister@gmail.com>
2021-01-04 08:47:32 +0100

    ..

--- build.rs
+++ build.rs
@@ -1,6 +1,7 @@
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     tonic_build::configure().out_dir("src/proto/").compile(
         &[
+            "src/proto/auth.proto",
             "src/proto/prelude.proto",
             "src/proto/user.proto",
             "src/proto/customer.proto",
--- src/proto/mod.rs
+++ src/proto/mod.rs
@@ -1,3 +1,4 @@
+pub mod auth;
 pub mod cash;
 pub mod customer;
 pub mod document;
